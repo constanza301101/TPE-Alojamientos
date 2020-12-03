653cce04f82ec991dbf6d810f2d15b0865df872a
constanza30101 <comartinez@alumnos.exa.unicen.edu.ar>
2020-12-02 22:32:07 -0300

    modif herlper

--- Helpers/AuthHelper.php
+++ Helpers/AuthHelper.php
@@ -7,11 +7,29 @@ class AuthHelper
     {
     }
     //array de sesion
-    function login($userFromDB)
+    /*function login($userFromDB)
     {
         session_start();
         $_SESSION['USUARIO'] = $userFromDB->usuario;
         $_SESSION['LAST_ACTIVITY'] = time();
+    }*/
+
+    public function login(){
+        if(!isset($_SESSION)){ 
+            session_start(); 
+            if (!isset($_SESSION['usuario'])){
+                header("Location: ".BASE_URL."login");
+                die();
+            }else{
+                // Pasados los 2 minutos de inactividad, se cierra automaticamente la sesión
+                if(isset($_SESSION['timeout']) && (time()-$_SESSION['timeout'] > 800)){
+                    header("Location: ".BASE_URL."logout");  
+                    die();
+                }
+                // De lo contrario, se actualiza el temporizador
+                $_SESSION['timeout'] = time();
+            }
+        }
     }
 
     function logout()
@@ -21,11 +39,46 @@ class AuthHelper
         header("Location: " . LOGIN);
     }
 
-    function checkLogIn()
-    {   
-        session_start();
-        if(!isset($_SESSION['USUARIO'])){
-            return false;
-        }else return true;
+   
+    static public function checkLogIn() { 
+        if (!isset($_SESSION)){
+            session_start();
+            if (isset($_SESSION['usuario'])) {
+                return true;
+            }       
+            else{
+                return false;
+            }
+        }else{
+            if (isset($_SESSION['usuario'])) {
+                return true;
+            }       
+            else{
+                return false;
+            }
+        }
+    }
+
+    public function isAdmin(){
+        if (!isset($_SESSION)){
+            session_start();
+            if (isset($_SESSION['usuario'])){
+                if($_SESSION['admin'] === "1"){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }else{
+            if (isset($_SESSION['usuario'])){
+                if($_SESSION['admin'] === "1"){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
     }  
  }
